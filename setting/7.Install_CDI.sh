b286c9c557f9318a7dc1a1e87160c7f2e095ba1f
lcw729 <lcw729729@gmail.com>
2022-01-18 13:12:02 +0900

    update setting shell script files

---
+++ setting/7.Install_CDI.sh
@@ -0,0 +1,4 @@
+kubectl create -f ../kubevirt/storage-setup.yml
+export VERSION=$(curl -s https://github.com/kubevirt/containerized-data-importer/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-cr.yaml
